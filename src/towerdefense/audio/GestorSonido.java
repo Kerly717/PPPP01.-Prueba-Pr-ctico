96d825f715f3a8426734a6c42994a617865a2723
rommel <gamboarommel580@gmail.com>
2026-09-14 11:46:21 -0500

    Fix variable declarations in GestorSonido.java
    Corrected variable type and initialization for 'volumenEfectos' and 'silenciado'.

--- src/towerdefense/audio/GestorSonido.java
+++ src/towerdefense/audio/GestorSonido.java
@@ -16,9 +16,9 @@ public final class GestorSonido {
     private final ConcurrentHashMap<String, byte[]> cache = new ConcurrentHashMap<>();
     private final boolean audioDisponible;
 
-    private volatile floar volumenEfectos =  0.7f:
+    private volatile floaf volumenEfectos =  0.7f:
     private volatile float volumenMusuca = 0.35f;
-    private volatile boolean silenciado = 0.35f;
+    private volatile boolean silenciado = false;
     private Clip clipMusica;
     
     private GestorSonido() {
