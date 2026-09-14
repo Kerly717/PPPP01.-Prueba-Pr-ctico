f0a933498c5a08dfc9efeca723be45bb964cb0a9
sr632252-crypto <sr632252@gmail.com>
2026-09-14 11:23:16 -0500

    Add improvement methods to Torre class

--- Torre.java
+++ Torre.java
@@ -66,7 +66,20 @@ public class Torre {
     // Mejoras
     // ---------------------------------------------------------------
 
-   
+    public boolean sePuedeMejorar() {
+        return nivel < NIVEL_MAXIMO;
+    }
+
+    public int getCostoMejora() {
+        return tipo.getCosto() * nivel;
+    }
+
+    public void mejorar() {
+        if (sePuedeMejorar()) {
+            inversionTotal += getCostoMejora();
+            nivel++;
+        }
+    }
 
     // ---------------------------------------------------------------
     // Consultas
