abb330f43c85923a0cf23d3db976a899fe113ee4
Kerly717 <reyesmargorh@gmail.com>
2026-09-14 11:23:13 -0500

    Implement LIFO stack with basic operations
    Added a LIFO stack implementation with methods for pushing, popping, and viewing the top element.

--- estructuras/Pila.java
+++ estructuras/Pila.java
@@ -1,5 +1,39 @@
 package towerdefense.estructuras;
+/**
+ * Pila LIFO (el ultimo en entrar es el primero en salir), enlazada.
+ *
+ * Se usa para el HISTORIAL DE ACCIONES: una pila guarda lo que el jugador hizo
+ * (deshacer) y otra lo que deshizo (rehacer).
+ *
+ * @param <T> tipo de dato almacenado.
+ */
+public class Pila<T> {
 
+    private NodoSimple<T> tope;
+    private int contador;
+
+    public void apilar(T dato) {
+        NodoSimple<T> nuevo = new NodoSimple<>(dato);
+        nuevo.setSiguiente(tope);
+        tope = nuevo;
+        contador++;
+    }
+
+    /** Extrae el tope, o null si la pila esta vacia. */
+    public T desapilar() {
+        if (estaVacia()) {
+            return null;
+        }
+        NodoSimple<T> salida = tope;
+        tope = tope.getSiguiente();
+        salida.setSiguiente(null);
+        contador--;
+        return salida.getDato();
+    }
+
+    public T verTope() {
+        return estaVacia() ? null : tope.getDato();
+    }
 
 
     public NodoSimple<T> getTope() {
