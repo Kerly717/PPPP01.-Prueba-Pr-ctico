2ac2b11470667ddf38bcf7d9dcf18e8b2ab9d7ce
rommel <gamboarommel580@gmail.com>
2026-09-14 11:49:09 -0500

    Add main method to launch application

--- src/towerdefense/Main.java
+++ src/towerdefense/Main.java
@@ -9,8 +9,12 @@ public final class Main {
     private Main() {
     }
 
- 
+    public static void main(String[] args) {
+        aplicarAspectoDelSistema();
+        SwingUtilities.invokeLater(() -> new VentanaPrincipal().setVisible(true));
+    }
 
+ 
     private static void aplicarAspectoDelSistema() {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
